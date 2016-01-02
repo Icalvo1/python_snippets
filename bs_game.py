7642e4c20e54c6fb1f64b4394a59a55f2a9fe277
icalvo1 <icalvo1@yahoo.com>
2016-01-02 15:55:36 -0800

    added function grid_clean

--- bs_game.py
+++ bs_game.py
@@ -12,6 +12,16 @@ def print_board(board):
 
 print_board(board)
 
+def grid_clean(num):
+    if num == 0:
+        return num + 1
+    elif num > 0 or num <=5:
+        return num - 1
+    elif isinstance(num, basestring) or num =='':
+        print 'guess must be a number'
+    else: 
+        return num
+
 def random_row(board):
     return randint(0, len(board) - 1)
 
@@ -20,23 +30,23 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-guess_row = int(raw_input("Guess Row:"))
-guess_col = int(raw_input("Guess Col:"))
-
 print ship_row
 print ship_col
+guess_row = grid_clean(int(raw_input("Guess Row:")))
+guess_col = grid_clean(int(raw_input("Guess Col:")))
+
 
 # Write your code below!
+
 if guess_row == ship_row and guess_col == ship_col:
     print "Congratulations! You sank my battleship!"
 elif board[guess_row][guess_col] == 'X':
     print "You guessed that one already."
+elif guess_row > range(5) or guess_col > range(5):
+    print "Oops, that's not even in the ocean."
 else:
-    print "You missed my battleship!"
     board[guess_row][guess_col] = 'X'
+    print "You missed my battleship!"
     print_board(board)
-    if guess_row > range(5) or guess_col > range(5):
-        print "Oops, that's not even in the ocean."
-    else:
-        print "You missed my battleship!"
-        board[guess_row][guess_col] = 'X'
+    print grid_clean(ship_row)
+    print grid_clean(ship_col)
