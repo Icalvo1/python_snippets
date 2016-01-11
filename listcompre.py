808176812f63fe835d651b85fec801ec0a0f6095
icalvo1 <icalvo1@yahoo.com>
2016-01-10 23:40:48 -0800

    added ex of list comprehension

---
+++ listcompre.py
@@ -0,0 +1,5 @@
+#example of list comprehension operation counting backwards by tens
+to_one_hundred = range(101)
+# Add your code below!
+backwards_by_tens = to_one_hundred[::-10]
+print backwards_by_tens
